0d5db168459a62fc5d01161f3b8bba5f471d9049
ali ouarrirh <aouarrirh@gmail.com>
2026-03-06 16:03:10 +0000

    fix: fix the cors policy origins

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS pour React en local
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:5173"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
